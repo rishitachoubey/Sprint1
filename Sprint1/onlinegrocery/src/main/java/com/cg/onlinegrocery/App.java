6c89f9ae62383a4891511b45dc286bac5a87f04f
rishitachoubey <rishitachoubey999@gmail.com>
2021-01-30 18:50:34 +0530

    test

--- Sprint1/onlinegrocery/src/main/java/com/cg/onlinegrocery/App.java
+++ Sprint1/onlinegrocery/src/main/java/com/cg/onlinegrocery/App.java
@@ -25,6 +25,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+  System.out.println( "Hello World!" );
     }
 }
 >>>>>>> 2c0e3db (test)
